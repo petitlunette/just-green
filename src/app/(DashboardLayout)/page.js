3322dbcc89d16e637a9d481279757bdca3867208
petitlunette <147147487+petitlunette@users.noreply.github.com>
2023-11-29 14:17:05 +0000

    Update page.js

--- src/app/(DashboardLayout)/page.js
+++ src/app/(DashboardLayout)/page.js
@@ -1,7 +1,45 @@
 'use client'
 import Head from "next/head";
 import { Col, Row } from "reactstrap";
-import Feeds from "@/app/(DashboardLayout)/components/dashboard/Feeds";
+import Feeds from "import React, { useEffect } from 'react';
+import Plotly from 'plotly.js-dist';
+import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+
+import ganttData from 'seasonal_produce.json'; 
+
+const GanttChart = () => {
+  useEffect(() => {
+    const processData = ganttData.map(item => {
+      return {
+        x: [item.Start, item.Finish],
+        y: [item.Task],
+        type: 'bar',
+        orientation: 'h',
+        // additional configuration if needed
+      };
+    });
+
+    const layout = {
+      // layout configuration
+    };
+
+    Plotly.newPlot('ganttChart', processData, layout);
+  }, []);
+
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle tag="h5">Gantt Chart</CardTitle>
+        <CardSubtitle className="text-muted" tag="h6">
+          Seasonal Produce Schedule
+        </CardSubtitle>
+        <div id="ganttChart" style={{ width: '100%', height: '390px' }} />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GanttChart;";
 import ProjectTables from "@/app/(DashboardLayout)/components/dashboard/ProjectTable";
 import TopCards from "@/app/(DashboardLayout)/components/dashboard/TopCards";
 import Blog from "@/app/(DashboardLayout)/components/dashboard/Blog";
